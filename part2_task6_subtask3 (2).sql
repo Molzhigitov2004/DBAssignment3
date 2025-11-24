4b3490f38ec3f79690c1145b086ceb9294542005
w-obi <eradillov04@gmail.com>
2025-11-24 17:09:29 +0500

    changes in the structure of the project

--- part2_task6_subtask3 (2).sql
+++
@@ -1,5 +0,0 @@
-SELECT u.given_name, u.surname, AVG(a.work_hours * c.hourly_rate) AS average_pay
-FROM APPOINTMENT AS a, CAREGIVER AS c, "USER" AS u
-WHERE u.user_id = c.caregiver_user_id AND a.caregiver_user_id = c.caregiver_user_id AND a.status = 'Accepted'
-GROUP BY u.user_id, u.given_name, u.surname
-ORDER BY u.given_name, u.surname;
--- part2_task8 (2).sql
+++
@@ -1,4 +0,0 @@
-SELECT given_name, surname, job_id, date_applied
-FROM "USER", JOB_APPLICATION
-WHERE user_id = caregiver_user_id
-ORDER BY given_name, surname, job_id, date_applied;
